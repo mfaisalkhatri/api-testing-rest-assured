31747912b53fb949149025ff2e3cabee0123c7c1
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-12-19 12:00:45 +0530

    test updated with allure annotations

--- src/test/java/io/github/mfaisalkhatri/restfulecommerce/tests/AddOrderTests.java
+++ src/test/java/io/github/mfaisalkhatri/restfulecommerce/tests/AddOrderTests.java
@@ -6,19 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.github.mfaisalkhatri.restfulecommerce.pojo.Order;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 public class AddOrderTests {
 
     @Test
+    @Severity (SeverityLevel.NORMAL)
+    @Description ("Create Order test with POST /addOrder API")
     public void testAddOrder () {
+        final List<Order> order = createOrderPayload ();
+        sendOrderRequest (order);
+    }
+
+    @Step ("Create order payload")
+    public List<Order> createOrderPayload () {
         final List<Order> orders = new ArrayList<> ();
         orders.add (new Order ("1", "34", "Canon Camera CX12", 14500, 1, 915, 15415));
-        given ().body (orders)
-            .when ()
-            .log ()
-            .all ()
+        return orders;
+    }
+
+    @Step ("Send POST order request and verify the response")
+    public void sendOrderRequest (final List<Order> orders) {
+        given ().when ()
+            .body (orders)
             .contentType (ContentType.JSON)
             .post ("http://localhost:3004/addOrder")
             .then ()
